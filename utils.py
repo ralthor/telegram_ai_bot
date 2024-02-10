96859d5933c1ea62f9ac52244313fb02cbca067b
ral <24351961+ralthor@users.noreply.github.com>
2024-02-10 22:33:25 +0000

    Add download_file function to utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import aiohttp
+
+async def download_file(file_path, ogg_filename):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file_path, allow_redirects=True) as response:
+            data = await response.read()
+            with open(ogg_filename, 'wb') as f:
+                f.write(data)
