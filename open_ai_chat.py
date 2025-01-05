fa5c86ac56929262c37144c2ea2a383e2a2617d2
ral <24351961+ralthor@users.noreply.github.com>
2025-01-05 00:58:43 +0000

    Add image support to openai chat class

--- open_ai_chat.py
+++ open_ai_chat.py
@@ -1,11 +1,17 @@
+import logging
 import os
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging = logging.getLogger(__name__)
+
 
 class OpenAIChat:
-    PROMPT = {"role": "system", "content": "You are a helpful assistant, responding in a friendly and informative manner, but don't go too long."}
-    def __init__(self, chat_limit=10, model="gpt-3.5-turbo"):
+    PROMPT = {"role": "system", "content": "You are a helpful assistant, responding in a friendly and informative manner. When possible, be short and to the point. If you don't know the answer, you can say so. Don't hilusinate or make things up."}
+    def __init__(self, chat_limit=30, model="gpt-4o-mini"):
         load_dotenv()
         self.client = AsyncOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
         self.users = {}
@@ -27,6 +33,7 @@ class OpenAIChat:
             model=model,
             messages=messages,
             )
+
         if user_id is not None:
             self.users[user_id] = messages + [{"role": "assistant", "content": response.choices[0].message.content}]
             if len(self.users[user_id]) > self.chat_limit:
@@ -45,15 +52,20 @@ class OpenAIChat:
             )
         audio_file.close()
         return transcript.text
+    
+    async def add_image(self, image_url, user_id):
+        if user_id not in self.users:
+            self.users[user_id] = [self.PROMPT]
+        self.users[user_id].append({"role": "user", "content": [{"type": "image_url", "image_url": {"url": image_url}}]})
 
 async def test_openai_chat():
     chat = OpenAIChat()
     user_id = "123"
-    print(await chat.generate("Who won the world series in 2020?", user_id))
-    print(await chat.generate("Where was it played?", user_id))
+    logging.info(await chat.generate("Who won the world series in 2020?", user_id))
+    logging.info(await chat.generate("Where was it played?", user_id))
 
     await chat.clear(user_id)
-    print(await chat.generate("Where was it played?", user_id))
+    logging.info(await chat.generate("Where was it played?", user_id))
 
 if __name__ == "__main__":
     import asyncio
